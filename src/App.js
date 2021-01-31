b2e2279f0ed39b5580cdf3134e50c27d3b2a148e
Deepak Verma <dpkvrm999@gmail.com>
2021-01-31 23:14:35 +0530

    added photo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo600.png';
 import './App.css';
 
 function App() {
@@ -6,16 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>
+          grumpy-copycat
+        </h2>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/gru_cop/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Instagram
         </a>
       </header>
     </div>
